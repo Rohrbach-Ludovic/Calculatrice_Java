801e76d9d9759dcbeaff1e2e82deafbf5076148b
Rohrbach-Ludovic <rohrbachludovic@gmail.Com>
2025-01-27 10:02:28 +0100

    calculatrice basique

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -1,3 +1,55 @@
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.SequencedCollection;
+
 public class Calculatrice {
 
+    // une fonction qui prend le calcul
+    // un fn qui extrait
+    // une fn calcul
+    String calcul = "";
+    List<String> b = new ArrayList<>();
+
+    public Object ajoute(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entrez votre calcul");
+        b.add(calcul);
+        return calcul = scanner.next();
+    }
+
+    public List<String> historique(){
+        return b;
+    }
+
+    public double calculer(String calcul) {
+
+        if (calcul.contains("+")) {
+            String[] parts = calcul.split("\\+");
+            return Double.parseDouble(parts[0].trim()) + Double.parseDouble(parts[1].trim());
+        } else if (calcul.contains("-")) {
+            String[] parts = calcul.split("-");
+            return Double.parseDouble(parts[0].trim()) - Double.parseDouble(parts[1].trim());
+        } else if (calcul.contains("*")) {
+            String[] parts = calcul.split("\\*");
+            return Double.parseDouble(parts[0].trim()) * Double.parseDouble(parts[1].trim());
+        } else if (calcul.contains("/")) {
+            String[] parts = calcul.split("/");
+            return Double.parseDouble(parts[0].trim()) / Double.parseDouble(parts[1].trim());
+        } else if (calcul.contains("%")) {
+            String[] parts = calcul.split("%");
+            return Double.parseDouble(parts[0].trim()) % Double.parseDouble(parts[1].trim());
+        } else {
+            throw new IllegalArgumentException("Le calcul entré n'est pas valide !");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Calculatrice{" +
+                "calcul='" + calcul + '\'' +
+                ", b=" + b +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Entrez ");
+        Calculatrice calc = new Calculatrice();
+        String leCalcul = (String) calc.ajoute();
+        System.out.println(calc.calculer(leCalcul));
+
+        System.out.println();
     }
 }
